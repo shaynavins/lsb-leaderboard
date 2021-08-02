098ecb8b0f96bf8550594306334a9c259e7bfa21
shaynavins <51338693+shaynavins@users.noreply.github.com>
2021-08-02 13:13:18 +0530

    Update main.js
    change

--- src/main.js
+++ src/main.js
@@ -7,15 +7,15 @@ require('@/assets/main.scss')
 
 Vue.config.productionTip = false
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCledp6rdDtEiZygl-oPiKrf_v5cbXO1KY",
-  authDomain: "lsb-leaderboard.firebaseapp.com",
-  databaseURL: "https://lsb-leaderboard.firebaseio.com",
-  projectId: "lsb-leaderboard",
-  storageBucket: "lsb-leaderboard.appspot.com",
-  messagingSenderId: "568167360548",
-  appId: "1:568167360548:web:40d9e90747e81ad42e50b1"
-};
+ var firebaseConfig = {
+    apiKey: "AIzaSyArjsLvmjcEWN4VlQ1F6qzQ8aUalrWuBdI",
+    authDomain: "lsbboard.firebaseapp.com",
+    projectId: "lsbboard",
+    storageBucket: "lsbboard.appspot.com",
+    messagingSenderId: "492496397386",
+    appId: "1:492496397386:web:41cbe093b202ecfc5ef2b3",
+    measurementId: "G-VF79SL0N4N"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
